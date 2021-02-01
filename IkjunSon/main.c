1f0f98066b620cf7042b445e8d4ef9b020d3b653
son0179 <starson2648@gmail.com>
2021-02-01 17:31:46 +0900

    로직은 완료...?

--- IkjunSon/main.c
+++ IkjunSon/main.c
@@ -2,8 +2,8 @@
 #include<string.h>
 #define DEST_DATE 4
 #define MAX
-#define INDEX 1
-#define VALUE 0
+#define ftell _ftelli64
+#define fseek _fseeki64
 #define DEFAULT_LENGTH 1024 * 6
 typedef struct date_index{
 	char value[31];
@@ -13,9 +13,11 @@ typedef struct date_index{
 unsigned long long moveFPLine(FILE *p , int n){
 	char tmp[DEFAULT_LENGTH];
 	int i;
-	for(i=0 ; i<n ; i++)
+	for(i=0 ; i<n ; i++){
 		fgets(tmp,DEFAULT_LENGTH,p);
-	return ftell(p);
+	}
+	i=ftell(p);
+	return i;
 }
 void getdate(char *c , FILE *p){
 	int i;
@@ -65,15 +67,17 @@ int main(){
 	char date[31];
 	unsigned long long datei[10]={0};
 	int times=1,i;
-	int debug=1;
-	int ck[10+1]={0};
+	int lastindex;
+	char ck[10+1];
 	unsigned long long headLine[10]={0};
 	unsigned long long endLine[10]={0};
 	int minI=0;
 	date_index priorQ[10+1];  // round Q로 구성 시도 해보자 
-	int priorQ_tail=0;
-	int priorQ_head=0;
-	
+	int priorQ_tail;
+	int priorQ_head;
+	unsigned long long FileSize,isEnd;
+	char tempfilename1 [] = "temp1.txt";
+	char tempfilename2 [] = "temp2.txt";
 	
 	
 	FILE *tp = fopen("ol_cdump_2020-11-30_001.txt","r");
@@ -83,60 +87,73 @@ int main(){
 		printf("check data");
 		return 0;
 	}
+	fseek(tp,0,SEEK_END);
+	FileSize = ftell(tp);
 	fclose(tp);
 	
-	tp = fopen("temp.txt","w");
+	tp = fopen("temp1.txt","w");
 	fp = fopen("ol_cdump_2020-11-30_001.txt","r");
 	
 	
-	
+	//do{
+	int kkk=0;
+	int nn=1;
 	do{
+		headLine[0] = 0;
+		memset(ck,0,sizeof(ck));
+		lastindex=9; 
+		priorQ_tail=0;
+		priorQ_head=0;
+		do{
+			
+			//priorQ 선행 작업 
+			for(i=0 ; i<10 ; i++){
+				if(ck[i]) break;
+				fseek(fp,headLine[0],SEEK_SET);
+				headLine[i] = moveFPLine(fp,i*times) ;
+				endLine[ (i+9)%10 ] = headLine[i] - 1 ; // index underflow 방지
 		
-		//priorQ 선행 작업 
-		for(i=0 ; i<10 ; i++){
-			//memset(date,0,sizeof(date));
-			//if(ck[i]) break;
-			fseek(fp,0,SEEK_SET);
-			headLine[i] = moveFPLine(fp,i*times) ;
-			endLine[ (i+9)%10 ] = headLine[i]-1; // index underflow 방지
-	
-			//if( feof(fp) ) ck[i+1] = 1;
-			printf("AAAAAA!!!!!!!!!!!");
-			
-			getdate(date,fp);
-			printf("\n%s\n",date);
-			pushPQ( priorQ , priorQ_tail++ , priorQ_head , date , i);
-			if(priorQ_tail > 10) priorQ_tail %= 10+1;
-		}
-		endLine[9] = ftell(fp)-1;
-		for(i=0 ; i<10 ; i++){
-			printf("%d range : %d ~ %d\n",i,headLine[i],endLine[i]);
-		}
-		
-		while(priorQ_tail != priorQ_head){
-			
-			minI=popPQ(priorQ,priorQ_head++);
-			if(priorQ_head > 10) priorQ_head%=10+1;
-			
-			fseek(fp, headLine[minI] ,SEEK_SET);
-			fgets(tmp,DEFAULT_LENGTH,fp);
-			/*
-			printf("index : %d\n",minI);
-			printf("head : %d\n",priorQ_head);
-			printf("tail : %d\n",priorQ_tail);
-			printf("\n\ntest\n%s\n\n",tmp);
-			*/
-			fprintf(tp,"%d : %s",debug,tmp);
-			if( !feof(fp) && ftell(fp) < endLine[minI] ){
-				printf("\n%d %d   %d\n",ftell(fp),endLine[minI],minI);
-				printf("\n!!debug %d !!\n\n",debug++);
-				headLine[minI] = ftell(fp);
+				if( feof(fp) ){
+					lastindex = i-1;
+					ck[i] = 1;
+					break;
+				}
+				
+				getdate(date,fp);
 				pushPQ( priorQ , priorQ_tail++ , priorQ_head , date , i);
-				if(priorQ_tail > 10) priorQ_tail %= 10+1;	
+				if(priorQ_tail > 10) priorQ_tail %= 10+1;
 			}
-		}
+			fgets(tmp,DEFAULT_LENGTH,fp);
+			endLine[9] = ftell(fp)-1;
+			
+			fseek(fp,headLine[0],SEEK_SET);
+			
+			while(priorQ_tail != priorQ_head){
+				
+				minI=popPQ(priorQ,priorQ_head++);
+				if(priorQ_head > 10) priorQ_head%=10+1;
+				
+				fseek(fp, headLine[minI] ,SEEK_SET);
+				fgets(tmp,DEFAULT_LENGTH,fp);
+				fprintf(tp,"%s",tmp);
+				
+				if( !feof(fp) && ftell(fp) < endLine[minI] ){
+					headLine[minI] = ftell(fp);
+					pushPQ( priorQ , priorQ_tail++ , priorQ_head , date , i);
+					if(priorQ_tail > 10) priorQ_tail %= 10+1;	
+				}
+			}
+			
+			headLine[0] = endLine[lastindex]+1;
+			fseek(fp,0,SEEK_END);
+			printf("%llu %llu\n\n",ftell(fp),headLine[0]);
+		}while( ftell(fp) > headLine[0] );
 		times*=10;
-	}while(0);
+		//fclose(fp);
+		//fclose(tp);
+		fseek(fp,0,SEEK_SET);
+		isEnd=moveFPLine(fp,times*10);
+	}while( FileSize > isEnd);
 	
 	fclose(tp);
 	fclose(fp);
