8423d1e671d79e019dedc62715fb96a57496befe
GiJO Son <autowiz@xslab.co.kr>
2021-03-15 16:21:03 +0900

    sort-program version 0.3.1

--- sort-program.py
+++ sort-program.py
@@ -2,24 +2,90 @@
 # -*- coding: utf-8 -*-
 
 import sys
-sys.setrecursionlimit(1000000000)
+#sys.setrecursionlimit(1000000000)
 
+call_quick_cnt = 0
+call_requick_cnt = 0
+gbl_i_loop_cnt = 0
+
+def bubble(a, start, end, sort_key):
+    global gbl_i_loop_cnt
+    swap_cnt = 0
+    i_loop_cnt = 0
+
+    if start < end :
+        for i in range ( start , end ) :
+            i_loop_cnt += 1
+            swap_cnt = 0
+            for j in range ( start , end ) :
+                if a[j][sort_key] > a[j+1][sort_key] :
+                    swap_cnt += 1
+                    a[j] , a[j+1] = a[j+1] , a[j]
+                # end if.
+            # end for loop ( j )
+
+            if swap_cnt == 0 :
+                # break for loop (i)
+                break
+            # end if 
+        # end for loop ( i )
+    # end if start < end
+
+    gbl_i_loop_cnt += i_loop_cnt
+    if sort_key == 3 :
+        print( "DEBUG: i_loop_cnt = " , i_loop_cnt , "\t(" , gbl_i_loop_cnt , ")" )
+    if sort_key == 4 :
+        print( "DEBUG: point_3: i_loop_cnt = " , i_loop_cnt , "\t(" , gbl_i_loop_cnt , ")" )
+        if gbl_i_loop_cnt % 1000 == 0 :
+            print( "DEBUG: point_2: i_loop_cnt = " , i_loop_cnt , "\t(" , gbl_i_loop_cnt , ")" )
+
+# end bubble sort function .
 
 def quick(a, start, end):
     global call_quick_cnt
     call_quick_cnt+=1
+    #swap_cnt=0
     if start < end: 
         left = start 
-        pivot = a[end][3]
-        for i in range(start, end): 
-            if a[i][3] < pivot: 
-                a[i], a[left] = a[left], a[i] 
-                left += 1 
-        a[left] , a[end] = a[end], a[left] 
+        pivot_pt = start + ( end - start ) // 2
+        pivot_value = a[pivot_pt][3]
+        # debug.
+        #print("DEBUG: point-1: start, pivot, end = " , start , pivot_pt , end )
+        for i in range(start, end+1): 
+            #print("DEBUG: i = " , i , " .")
+            if a[i][3] < pivot_value: 
+                if i != left :
+                    #swap_cnt+=1
+                    a[i], a[left] = a[left], a[i] 
+                if left == pivot_pt :
+                    pivot_pt = i
+                left += 1
+                #if left > end :
+                #    #print("ERROR: left is too big ( left: {} , end: {} ).".format(left,end))
+                #    sys.stderr.write("ERROR: left is too big ( left: {} , end: {} ).\n".format(left,end))
+                    
+        # end for loop
+
+        #print("DEBUG: point-2: start, pivot, end, i, left = " , start , pivot_pt , end , i , left )
+
+        if a[left][3] > pivot_value :
+            #swap_cnt+=1
+            #if left >= pivot_pt :
+            #    sys.stderr.write("ERROR: left -ge pivot_pt , ( {} , {} ).\n".format(left,pivot_pt))
+            ##end debug print if
+            a[left] , a[pivot_pt] = a[pivot_pt], a[left] 
+
+        #if swap_cnt != 0 :
+        #    quick(a, start, left-1)
+        #    quick(a, left+1, end)
+        # end if ( swap_cnt )
+        
         quick(a, start, left-1)
         quick(a, left+1, end)
 
 def requick(a, start, end):
+    global call_requick_cnt
+    call_requick_cnt += 1
     if start < end: 
         left = start 
         pivot = a[end][4]
@@ -33,9 +99,12 @@ def requick(a, start, end):
 
 #원본 파일 분활 
 def  split():
+    global call_quick_cnt
+    global call_requick_cnt
     p=0
     q=0
-    origin_file = open('ol_cdump_2020-11-30.txt','r',encoding='UTF-8')
+    #origin_file = open('ol_cdump_2020-11-30.txt','r',encoding='UTF-8')
+    origin_file = open('4m_lines_part_sorted.txt','r',encoding='UTF-8')
 
     for ne in range(fi_num):
         op = 'sort_%d.txt'%(ne+1)
@@ -52,8 +121,11 @@ def  split():
             str_file.append(list_file[coun].split())
             coun=coun+1
 
-        print("DEBUG: call_quick_cnt = " , call_quick_cnt)
-        quick(str_file, 0, len(str_file)-1)
+        #quick(str_file, 0, len(str_file)-1)
+        #bubble(str_file, 0, len(str_file)-1)
+        bubble(str_file, 0, len(str_file)-1, 3)
+
+        #print("DEBUG: call_quick_cnt = %d" , call_quick_cnt )
 
         for x in range(coun): 
             in_file[fi].write(str(fi+1))
@@ -64,13 +136,16 @@ def  split():
             in_file[fi].write("\n")
         if (fi==p):
             p=p+(fi_num/10)
-            q=q+10
             print (q,"% 작성완료(파일)")
+            q=q+10
+    # end for loop ( fi ).
 
-    origin_file.close
+    origin_file.close()
 
     for ne in range(fi_num):
         in_file[ne].close()
+
+    print (q,"% 작성완료(파일).")
 
 def combine():
     print ("2311345")
@@ -96,7 +171,8 @@ def combine():
             coun=coun+1
         w_list.append(str_file[0])
 
-    requick(w_list, 0, len(w_list)-1)
+    #requick(w_list, 0, len(w_list)-1)
+    bubble(w_list, 0, len(w_list)-1, 4)
         
     reinput_file = open('sort.txt','w',encoding='UTF-8')
     for z in range(len(w_list[0])-1):
@@ -104,7 +180,7 @@ def combine():
         reinput_file.write("   ")
     reinput_file.write("\n")
 
-    reout_file.close
+    reout_file.close()
 
     for ne in range(fi_num):
         in_file[ne].close()
@@ -144,7 +220,8 @@ def combine():
         except:
             next
 
-        requick(w_list, 0, len(w_list)-1)
+        #requick(w_list, 0, len(w_list)-1)
+        bubble(w_list, 0, len(w_list)-1, 4)
 
         if(q==stop):
             q=q+(fi_num*line_num/10)
@@ -163,8 +240,6 @@ def combine():
         lain_file[ne].close()
     laout_file.close()
     
-call_quick_cnt = 0
-call_requick_cnt = 0
 fi_num = 2000 #파일 갯수
 line_num = 2000 # 나눌 줄수
 in_file=[] #
@@ -175,9 +250,11 @@ w_list=[]
 
 
 split()
-print("INFO: call_quick_cnt = " , call_quick_cnt)
-print("INFO: call_requick_cnt = " , call_requick_cnt)
 
-#combine()
-print("INFO: call_quick_cnt = " , call_quick_cnt)
-print("INFO: call_requick_cnt = " , call_requick_cnt)
+print("INFO: call_quick_cnt = %d" , call_quick_cnt)
+print("INFO: call_requick_cnt = %d" , call_requick_cnt)
+
+combine()
+
+print("INFO: call_quick_cnt = %d" , call_quick_cnt)
+print("INFO: call_requick_cnt = %d" , call_requick_cnt)
